4de6aa2129fffcd9ed0b1a80bdcceddf237e57a3
Graham Hukill <ghukill@gmail.com>
2018-07-06 12:47:38 -0400

    breaking out mysql

--- templates/combine.sql
+++ templates/combine.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE combine CHARACTER SET utf8 COLLATE utf8_general_ci;;
-CREATE USER 'combine'@'localhost' IDENTIFIED BY 'combine';
-GRANT ALL PRIVILEGES ON * . * TO 'combine'@'localhost';
+CREATE USER 'combine'@'%' IDENTIFIED BY 'combine';
+GRANT ALL PRIVILEGES ON * . * TO 'combine'@'%';
 FLUSH PRIVILEGES;
